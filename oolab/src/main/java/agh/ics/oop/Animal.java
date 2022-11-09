a7efa259b06518b21536acc55f551aa037ea745a
NiEkOKsU <93381337+NiEkOKsU@users.noreply.github.com>
2022-11-09 20:35:44 +0100

    lab4

--- oolab/src/main/java/agh/ics/oop/Animal.java
+++ oolab/src/main/java/agh/ics/oop/Animal.java
@@ -1,8 +1,15 @@
 package agh.ics.oop;
 
+import java.util.Objects;
+
 public class Animal {
     private MapDirection direction = MapDirection.NORTH;
     private Vector2d position = new Vector2d(2,2);
+
+    public Animal(IWorldMap map){
+
+    }
+
 
     public Vector2d getPosition() {
         return position;
@@ -27,69 +34,22 @@ public class Animal {
     }
 
 
-    public boolean isAt(Vector2d position){
-        return this.position.x == position.x && this.position.y == position.y;
+    public boolean isAt(Vector2d checkPosition){
+        return Objects.equals(position, checkPosition);
     }
 
 
     public void move(MoveDirection direction){
-        Vector2d newVector = null;
         Vector2d upright = new Vector2d(4, 4);
         Vector2d downleft = new Vector2d(0, 0);
-        if (direction == MoveDirection.FORWARD) {
-            switch (this.direction) {
-                case NORTH -> {
-                    newVector = new Vector2d(this.position.x + MapDirection.NORTH.toUnitVector()[0], this.position.y + MapDirection.NORTH.toUnitVector()[1]);
-                }
-                case SOUTH -> {
-                    newVector = new Vector2d(this.position.x + MapDirection.SOUTH.toUnitVector()[0], this.position.y + MapDirection.SOUTH.toUnitVector()[1]);
-                }
-                case WEST -> {
-                    newVector = new Vector2d(this.position.x + MapDirection.WEST.toUnitVector()[0], this.position.y + MapDirection.WEST.toUnitVector()[1]);
-                }
-                case EAST -> {
-                    newVector = new Vector2d(this.position.x + MapDirection.EAST.toUnitVector()[0], this.position.y + MapDirection.EAST.toUnitVector()[1]);
-                }
-            }
+        switch (direction){
+            case LEFT -> this.direction = this.direction.previous();
+            case RIGHT -> this.direction = this.direction.next();
+            case FORWARD -> this.position = this.position.add(this.direction.toUnitVector());
+            case BACKWARD -> this.position = this.position.substract(this.direction.toUnitVector());
         }
-        else if (direction == MoveDirection.BACKWARD){
-            switch (this.direction) {
-                case NORTH -> {
-                    newVector = new Vector2d(this.position.x + MapDirection.SOUTH.toUnitVector()[0], this.position.y + MapDirection.SOUTH.toUnitVector()[1]);
-                }
-                case SOUTH -> {
-                    newVector = new Vector2d(this.position.x + MapDirection.NORTH.toUnitVector()[0], this.position.y + MapDirection.NORTH.toUnitVector()[1]);
-                }
-                case WEST -> {
-                    newVector = new Vector2d(this.position.x + MapDirection.EAST.toUnitVector()[0], this.position.y + MapDirection.EAST.toUnitVector()[1]);
-                }
-                case EAST -> {
-                    newVector = new Vector2d(this.position.x + MapDirection.WEST.toUnitVector()[0], this.position.y + MapDirection.WEST.toUnitVector()[1]);
-                }
-            }
-        }
-        else if (direction == MoveDirection.LEFT || direction == MoveDirection.RIGHT){
-            newVector = new Vector2d(this.position.x, this.position.y);
-        }
-        switch (this.direction){
-            case NORTH, EAST -> checkBound(direction, newVector, newVector.precedes(upright), newVector.follows(downleft));
-            case SOUTH, WEST -> checkBound(direction, newVector, newVector.follows(downleft), newVector.precedes(upright));
-        }
+        this.position = this.position.upperRight(downleft);
+        this.position = this.position.lowerLeft(upright);
     }
 
-
-    public void checkBound(MoveDirection direction, Vector2d newVector, boolean precedes, boolean follows) {
-        if (direction == MoveDirection.FORWARD && precedes){
-            setPosition(new Vector2d(newVector.x, newVector.y));
-        }
-        else if (direction == MoveDirection.BACKWARD && follows) {
-            setPosition(new Vector2d(newVector.x, newVector.y));
-        }
-        else if (direction.equals(MoveDirection.RIGHT)) {
-            setDirection(this.direction.next());
-        }
-        else if (direction.equals(MoveDirection.LEFT)){
-            setDirection(this.direction.previous());
-        }
-    }
 }
--- oolab/src/main/java/agh/ics/oop/OptionParser.java
+++ oolab/src/main/java/agh/ics/oop/OptionParser.java
@@ -1,19 +1,33 @@
 package agh.ics.oop;
 
+import java.util.Arrays;
+
 public class OptionParser {
     public MoveDirection[] parser(String[] moves){
         MoveDirection[] moves2 = new MoveDirection[moves.length];
         int i = 0;
         for (String move: moves){
             switch (move) {
-                case "b", "backward" -> moves2[i] = MoveDirection.BACKWARD;
-                case "f", "forward" -> moves2[i] = MoveDirection.FORWARD;
-                case "l", "left" -> moves2[i] = MoveDirection.LEFT;
-                case "r", "right" -> moves2[i] = MoveDirection.RIGHT;
-            };
-            i += 1;
+                case "b", "backward" ->{
+                    moves2[i] = MoveDirection.BACKWARD;
+                    i += 1;
+                }
+                case "f", "forward" -> {
+                    moves2[i] = MoveDirection.FORWARD;
+                    i += 1;
+                }
+                case "l", "left" -> {
+                    moves2[i] = MoveDirection.LEFT;
+                    i += 1;
+                }
+                case "r", "right" -> {
+                    moves2[i] = MoveDirection.RIGHT;
+                    i += 1;
+                }
+            }
+
         }
-        return moves2;
+        return Arrays.copyOf(moves2, i);
     }
 
 }
